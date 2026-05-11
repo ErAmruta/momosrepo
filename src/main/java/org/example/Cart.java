760b59226539675d0b5d551eb1148319adbdfbff
Gajanan <gajanan@gmail>
2026-05-11 14:08:27 +0530

    Revert "Merge pull request #5 from ErAmruta/momos-9988-mapping-issue-solved"
    This reverts commit fdd42b2187e7739d7d52fc941a736170fdedade7, reversing
    changes made to e563364663e51ea479501eaab71f39dadfc47dac.

--- src/main/java/org/example/Cart.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Cart
-{
-    public void cartShow()
-    {
-        System.out.println("Cart is showing in Cart class");
-    }
-}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,5 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("good morning");
-        Demo d=new Demo();
-        d.show();
     }
 }
