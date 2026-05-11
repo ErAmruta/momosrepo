a705fa02807aa1d3ee6c34f02fb67382b0bbde97
Gajanan <gajanan@gmail>
2026-05-11 23:28:34 +0530

    added cart and checkout

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("good morning");
+        Cart c=new Cart();
+        c.showCart();
     }
 }
